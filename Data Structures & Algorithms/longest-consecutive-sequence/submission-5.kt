7b5ea5ad0255138dccb682a9f8cb142243f5b56c
Thomas Sunderland <tsunderland77@gmail.com>
2026-06-04 12:37:43 -0400

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.kt
@@ -0,0 +1,29 @@
+class Solution {
+    fun longestConsecutive(nums: IntArray): Int {
+        if (nums.size == 0) return 0
+
+        val numsSet = nums.toHashSet()
+        val considerations = mutableListOf<Int>()
+
+        for (num in numsSet) {
+            if (!numsSet.contains(num - 1)) {
+                considerations.add(num)
+            }
+        }
+
+        //println("Considerations: $considerations, SetCount: ${numsSet.size}")
+
+        var maxConsecutive = 0
+        for (consideration in considerations) {
+            var count = 1
+            while (numsSet.contains(consideration + count)) {
+                count++
+            }
+
+            if (count > maxConsecutive) maxConsecutive = count
+            if (count > (numsSet.size - count)) return count
+        }
+
+        return maxConsecutive
+    }
+}
