bb5e949f898116f578b3c5e4079d7a7eac1b6940
Thomas Sunderland <tsunderland77@gmail.com>
2026-07-16 19:25:57 -0400

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        val numFrequency = mutableMapOf<Int, Int>()
+
+        nums.sort()
+
+        var index = 0
+        while (index < nums.size) {
+            val num = nums[index]
+            val lastIndex = nums.lastIndexOf(num)
+            val occurrences = lastIndex - index + 1
+
+            numFrequency[num] = occurrences
+            
+            index = lastIndex + 1
+        }
+
+        val topValues = numFrequency.values.sorted().takeLast(k)
+        return numFrequency.filterValues { it in topValues }.keys.toIntArray()
+    }
+}
