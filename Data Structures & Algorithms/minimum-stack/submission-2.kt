69cad142b6714a38d89776fd48ff2f213ef32b01
Thomas Sunderland <tsunderland77@gmail.com>
2026-07-17 11:32:55 -0400

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.kt
@@ -0,0 +1,30 @@
+class MinStack() {
+    private var currentMin: Int? = null
+    private val stack = mutableListOf<Pair<Int, Int>>()
+
+    fun push(value: Int) {
+        if (stack.isEmpty()) {
+            currentMin = value
+            stack.add(value to value)
+        } else {
+            if (getMin() < value) {
+                stack.add(value to getMin())
+            } else {
+                currentMin = value
+                stack.add(value to value)
+            }
+        }
+    }
+
+    fun pop() {
+        stack.removeLast()
+    }
+
+    fun top(): Int {
+        return stack.last().first
+    }
+
+    fun getMin(): Int {
+        return stack.last().second
+    }
+}
